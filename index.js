f60152485c8f13aef988a2006319676f060ffbf4
Nicole L. Hopkins <nicolehopkins@pursuit.org>
2018-10-10 19:51:31 -0400

    adding notes for 10-10

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+//FUNCTION DECLARATION
 const fifthPow = (num) => {
     return num ** 5;
 }
 
 console.log(fifthPow(2)) // 32
+
+//FUNCTION EXPRESION ANNON or FAT ARROW
+const fifthPow = (num) => {
+    return num ** 5;
+}
+
+//or
+const fifthPow = (num) => (num ** 5);
+
+console.log(fifthPow(2))
+
+//NAMED FUNCTION EXPRESSION
+const fifthPow = function someRandomNamelol(num) {
+
+}
